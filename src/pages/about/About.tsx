08619fc08133de21cc79d7cd1ff239548bae1a8c
Jeanaica Suplido <sjeanaica@gmail.com>
2022-06-04 20:10:25 +0800

    📝 Update content

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -14,8 +14,8 @@ const About: FC = () => {
         header='About'>
         <div className={styles.text}>
           <p>
-            Jeanaica Suplido is a Frontend Software Engineer which specializes
-            in React and Angular. She brings beautiful wireframes into working
+            Jeanaica Suplido is a Software Engineer which specializes in
+            Frontend Development. She brings beautiful wireframes into working
             applications in either mobile, web, and/or desktop.
           </p>
           <br />
@@ -23,9 +23,10 @@ const About: FC = () => {
             Jeanaica also enjoys travelling, painting, singing, and writing.
           </p>
           <br />
+          <p>Want someone to work on your website?</p>
+          <br />
           <p>
-            Want someone to work on your website? Email her{' '}
-            <Anchor href='mailto:sjeanaica@gmail.com'>here.</Anchor>
+            Email her <Anchor href='mailto:sjeanaica@gmail.com'>here.</Anchor>
           </p>
         </div>
       </Title>
